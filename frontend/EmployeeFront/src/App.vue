fb1c24d5f4397de810002203c5359c6e1f4102f4
CesarAybar <20240096@itla.edu.do>
2025-11-24 00:20:24 -0400

    changed frontend from ts to js

--- frontend/EmployeeFront/src/App.vue
+++ frontend/EmployeeFront/src/App.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="I need to remember how this works" />
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
