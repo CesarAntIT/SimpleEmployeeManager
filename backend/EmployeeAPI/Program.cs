793f03ab7afd818e4bea3e98086b7c9b381365cd
CesarAybar <20240096@itla.edu.do>
2025-11-23 22:49:04 -0400

    Set up EnityFramework and Scalar API Testing

--- backend/EmployeeAPI/Program.cs
+++ backend/EmployeeAPI/Program.cs
@@ -1,4 +1,6 @@
 
+using Scalar.AspNetCore;
+
 namespace EmployeeAPI
 {
     public class Program
@@ -19,6 +21,7 @@ namespace EmployeeAPI
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
